80176911219c332135302e6045aec212fce0d0f8
Rawa Aref <rawa_aref@hotmail.com>
2024-06-25 20:40:59 +0200

    add transaction component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
-const HomePage = () => {
+import Guest from "@/components/Guest";
+import AddTransaction from "@/components/AddTransaction";
+import { currentUser } from "@clerk/nextjs/server";
+const HomePage = async () => {
+  const user = await currentUser();
+
+  if(!user) {
+    return <Guest />
+  }
   return (<main>
-    <h1>Expense Tracker</h1>
+    <h1>Welcome, {user.firstName}</h1>
+    <AddTransaction />
   </main>);
 }
  
